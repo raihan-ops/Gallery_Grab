edd41af0f138ade28a8d8834cdcfb8ea463fc23d
raihan-urelaa-git <tasnimraihan@urelaa.com>
2024-06-14 20:35:57 +0600

    changes view port

--- index.js
+++ index.js
@@ -1,100 +1,102 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const puppeteer = require('puppeteer');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const puppeteer = require("puppeteer");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const getQuotes = async (url) => {
-    const browser = await puppeteer.launch({ 
-        headless: true ,
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-          ],
-          executablePath:
-            process.env.NODE_ENV === "production"
-              ? process.env.PUPPETEER_EXECUTABLE_PATH
-              : puppeteer.executablePath(),
+  const browser = await puppeteer.launch({
+    // headless: true ,
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  console.log("Browser launched.");
+
+  const page = await browser.newPage();
+  console.log("New page created.");
+  try {
+    await page.goto(url, { waitUntil: "networkidle2" });
+    console.log(`Navigated to ${url}`);
+    await page.setViewport({
+      width: 1200,
+      height: 5000,
     });
-    const page = await browser.newPage();
+    console.log("Viewport set.");
+    await autoScroll(page);
 
-    try {
-        await page.goto(url, { waitUntil: 'networkidle2' });
+    const imgSrcs = await page.evaluate(() => {
+      const imgs = document.querySelectorAll("picture img");
+      return Array.from(imgs).map((img) => img.getAttribute("src"));
+    });
 
-        await page.setViewport({
-            width: 1200,
-            height: 10000
-        });
-
-        await autoScroll(page);
-
-        const imgSrcs = await page.evaluate(() => {
-            const imgs = document.querySelectorAll('picture img');
-            return Array.from(imgs).map(img => img.getAttribute('src'));
-        });
-
-        return imgSrcs;
-    } catch (error) {
-        throw error; // Propagate error to be handled by caller
-    } finally {
-        await browser.close();
-    }
+    return imgSrcs;
+  } catch (error) {
+    throw error; // Propagate error to be handled by caller
+  } finally {
+    await browser.close();
+  }
 };
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/scrape', async (req, res) => {
-    const { url } = req.body;
-    try {
-        const data = await getQuotes(url);
-        res.json(data);
-    } catch (error) {
-        console.error('Error during scraping:', error);
-        res.status(500).json({ error: 'Failed to scrape data from URL' });
-    }
+app.post("/scrape", async (req, res) => {
+  const { url } = req.body;
+  try {
+    const data = await getQuotes(url);
+    res.json(data);
+  } catch (error) {
+    console.error("Error during scraping:", error);
+    res.status(500).json({ error: "Failed to scrape data from URL" });
+  }
 });
 
 async function autoScroll(page) {
-    await page.evaluate(async () => {
-        await new Promise((resolve) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
 
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
     });
+  });
 }
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Handle process events to ensure server stability
-process.on('unhandledRejection', (error, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', error);
-    // Handle the error or log it
+process.on("unhandledRejection", (error, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", error);
+  // Handle the error or log it
 });
 
-process.on('uncaughtException', (error, origin) => {
-    console.error('Uncaught Exception:', error, 'origin:', origin);
-    // Handle the error or log it
+process.on("uncaughtException", (error, origin) => {
+  console.error("Uncaught Exception:", error, "origin:", origin);
+  // Handle the error or log it
 });
 
 // Example of using pm2 to manage the server process:
